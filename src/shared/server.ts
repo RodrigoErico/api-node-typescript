ad4f4dd8e6b71df7e5f6062737767217cb674c9e
RodrigoErico <rodrigoerico25@gmail.com>
2021-08-25 20:42:54 -0400

    Configurado a rota

--- src/shared/server.ts
+++ src/shared/server.ts
@@ -1,2 +1,14 @@
 import express from 'express';
 import cors from 'cors';
+import routes from './http/routes';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(3333, () => {
+  console.log('Server started on port 3333! 🎈');
+});
